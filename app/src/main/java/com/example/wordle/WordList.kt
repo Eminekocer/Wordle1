faf3e9a915cdc4966bbcb147de3f57923b84a4bc
Growlitthe <157486191+Growlitthe@users.noreply.github.com>
2025-05-25 16:50:12 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/wordle/WordList.kt
@@ -0,0 +1,8 @@
+package com.mertatmaca.worldeapp
+
+object WordList { //wordle için oluşturulmuş deneme sınıfı
+    val words = listOf(
+        "apple", "grape", "house", "plane", "smile",
+        "tiger", "light", "bread", "table", "flame"
+    )
+}
